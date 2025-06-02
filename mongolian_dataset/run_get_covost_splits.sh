d5eb9bf96b810ba798bf0611372821c372de4113
triphamday <phamtri1232004@gmail.com>
2025-06-02 11:00:26 +0700

    initial commit

---
+++ mongolian_dataset/run_get_covost_splits.sh
@@ -0,0 +1,6 @@
+python /mnt/c/ComSL/mongolian_dataset/get_covost_splits.py \
+    --version 2 \
+    --src-lang mn \
+    --tgt-lang en \
+    --root /mnt/c/ComSL/mongolian_dataset \
+    --cv-tsv /mnt/c/ComSL/mongolian_dataset/validated.tsv
